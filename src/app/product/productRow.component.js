9d17d5c433ea28c615ca8f62b972f49636a071a9
Prima Winangun <primawinangun@gmail.com>
2018-02-06 16:45:40 +0800

    pop up setting

--- src/app/product/productRow.component.js
+++ src/app/product/productRow.component.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
+import ReactModal from 'react-modal';
 
 class ProductRow extends Component{
 	constructor(){
 		super();
 		this.state = {
-			selectedProduct: ''
+			selectedProduct: '',
+			showModal: false
 		}
+		
+		this.handleOpenModal = this.handleOpenModal.bind(this);
+		this.handleCloseModal = this.handleCloseModal.bind(this);
 	}
 	
+	handleOpenModal () {
+    this.setState({ showModal: true });
+	}
+	  
+	handleCloseModal () {
+		this.setState({ showModal: false });
+	}
+	
+	deleteProduct(id){
+		return(
+			<div>success</div>
+		)
+	}
+	
+	
 	render() {
+		const padding = 90; // adjust this to your needs
+		let height = (this.state.contentHeight + padding);
+		let heightPx = height + 'px';
+		let heightOffset = height / 2;
+		let offsetPx = heightOffset + 'px';
+		const style = {
+		  
+		};
 		var selectedProduct = '/product_detail/'+this.props.product.id;
 		var deleteProduct = '/product_delete/'+this.props.product.id;
 		var editProduct = '/product_update/'+this.props.product.id;
@@ -25,9 +53,39 @@ class ProductRow extends Component{
                 <a href={editProduct}
                     className='btn btn-primary m-r-1em'> Edit
                 </a>
-                <a href={deleteProduct}
-                    className='btn btn-warning m-r-1em'> Delete
-                </a>
+				<button onClick={this.handleOpenModal} className='btn btn-warning m-r-1em'>Delete</button>
+				<ReactModal 
+					isOpen={this.state.showModal}
+					contentLabel="onRequestClose Example"
+					style={{
+						overlay: {
+						position: 'fixed',
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						backgroundColor: 'rgba(255, 255, 255, 0.75)'
+					  },
+					  content: {
+						border: '1px solid #ccc',
+						borderRadius: '4px',
+						bottom: 'auto',
+						height: heightPx,  // set height
+						left: '30%',
+						padding: '2rem',
+						position: 'fixed',
+						right: 'auto',
+						top: '30%', // start from center
+						transform: 'translate(-50%,-' + offsetPx + ')', // adjust top "up" based on height
+						width: '40%',
+						maxWidth: '40rem'
+					  }
+					}}
+				>
+				  <p>Anda Yakin ingin menghapus item '{this.props.product.name}'</p>
+				  <button onClick={this.deleteProduct(this.props.product.id)} className='btn btn-warning'>Yakin</button>
+				  <button onClick={this.handleCloseModal} className='btn btn-warning m-r-1em'>Batal</button>
+				</ReactModal>
             </td>
         </tr>
         );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './app/assets/css/custom.css';
 import MainApp from './app/main.component.js';
 import registerServiceWorker from './registerServiceWorker';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#content');
 
 const App = () => (
     <MainApp />
